d6d9d0f14bc1dcf0562a94664808cdf515d9f759
Kebigon <git@kebigon.xyz>
2020-08-30 20:31:51 +0900

    Add a couple of scripts

---
+++ .local/bin/morphman_clean.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm ~/.local/share/Anki2/User\ 1/dbs/all.db
+rm ~/.local/share/Anki2/User\ 1/dbs/known.db
+rm ~/.local/share/Anki2/User\ 1/dbs/mature.db
+rm ~/.local/share/Anki2/User\ 1/dbs/seen.db
